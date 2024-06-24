1ea257c131b028e6c69a29a91f1fe31c382a963b
duressa-feyissa <duresa.feyisa.gose@gmail.com>
2024-06-24 23:40:27 +0300

    :bug: fix internal error

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,53 @@
-import { Box, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  Icon,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { MdErrorOutline } from "react-icons/md"; // Make sure to install 'react-icons' if not already
 
 const ErrorPage = () => {
+  const bgColor = useColorModeValue("gray.100", "gray.900");
+  const color = useColorModeValue("gray.800", "white");
+  const buttonBgColor = useColorModeValue("purple.500", "purple.300");
+
   return (
-    <Box
-      height={"100vh"}
-      width={"100%"}
-      display={"flex"}
-      justifyContent={"center"}
+    <Center
+      height="100vh"
+      width="100%"
+      bg={bgColor}
+      color={color}
+      transition="all 0.5s ease"
     >
-      <Box>
-        <Heading>Something Wrong!</Heading>
+      <Box textAlign="center" p={8} rounded="lg" shadow="2xl" borderWidth="1px">
+        <Icon
+          as={MdErrorOutline}
+          w={10}
+          h={10}
+          mb={4}
+          color={useColorModeValue("red.500", "red.300")}
+        />
+        <Heading mb={4}>Oops! Something Went Wrong</Heading>
+        <Text fontSize="lg" mb={8}>
+          We're having trouble loading the page you requested. Please try
+          reloading the page.
+        </Text>
+        <Button
+          backgroundColor={buttonBgColor}
+          color="white"
+          _hover={{
+            bg: useColorModeValue("purple.600", "purple.400"),
+          }}
+          size="lg"
+          onClick={() => window.location.reload()}
+        >
+          Try Again
+        </Button>
       </Box>
-    </Box>
+    </Center>
   );
 };
 
